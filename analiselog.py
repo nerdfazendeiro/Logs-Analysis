57a9c8a8a3170939a12a5ae0dbd47f0518a9c4e9
Rafael Ramalho Silveira <rafael_silveira125@hotmail.com>
2018-11-08 01:29:38 -0300

    Exibindo os artigos mais populares

--- analiselog.py
+++ analiselog.py
@@ -7,3 +7,8 @@ from analiselog_db import erro_requisicao
 titulo = artigo_maisacessado()
 artigo = autores_maispopulares()
 error = erro_requisicao()
+
+print("Quais são os três artigos mais populares de todos os tempos?\n")
+for i in range(0, len(titulo)):
+    print (titulo[i][0], " - ", titulo[i][1])
+print("\n")
