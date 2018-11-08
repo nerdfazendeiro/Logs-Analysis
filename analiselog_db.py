817e90f5d606b66c418793db510e04d60d7816cc
Rafael Ramalho Silveira <rafael_silveira125@hotmail.com>
2018-11-08 01:25:17 -0300

    Função que seleciona os autores mais populares

--- analiselog_db.py
+++ analiselog_db.py
@@ -12,3 +12,14 @@ def artigo_maisacessado():
     order by count(a.title) desc limit 3")
     return c.fetchall()
     db.close()
+    
+def autores_maispopulares():
+    """Função que retorna os autores de artigos mais populares de todos os
+    tempos"""
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute("select at.name, count(l.path) as view from articles as a join \
+    log as l on substring(path from 10)=a.slug join authors as at \
+    on at.id=a.author group by at.name order by count(at.name) desc")
+    return c.fetchall()
+    db.close()
