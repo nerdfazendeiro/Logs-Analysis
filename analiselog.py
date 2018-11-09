f97e934eb5d1dd2683ff63d5691a39944f852788
Rafael Ramalho Silveira <rafael_silveira125@hotmail.com>
2018-11-09 01:07:49 -0300

    Atualizando os Formats

--- analiselog.py
+++ analiselog.py
@@ -10,14 +10,14 @@ artigo = autores_maispopulares()
 error = erro_requisicao()
 
 print("Quais são os três artigos mais populares de todos os tempos?\n")
-for i in range(0, len(titulo)):
-    print (titulo[i][0], " - ", titulo[i][1])
+for row in titulo:
+    print('"{}" -- {} views'.format(row[0], row[1]))
 print("\n")
 print("Quem são os autores de artigos mais populares de todos os tempos?\n")
-for i in range(0, len(artigo)):
-    print (artigo[i][0], " - ", artigo[i][1])
+for row in artigo:
+    print('"{}" -- {} views'.format(row[0], row[1]))
 print("\n")
 print("Em quais dias mais de 1% das requisições resultaram em erros?\n")
-for i in range(0, len(error)):
-    print (error[i][0], " - ", error[i][1])
+print("Em qual dia teve a maior porcentagem de erros?\n")
+print('Day: "{}" -- {}'.format(error[0][0], error[0][1]))
 print("\n")
